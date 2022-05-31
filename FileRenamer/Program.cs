438fa3a89ad45d6f8aae5aa1c1da6d9fb1c265fa
acidrod <alarcon.rodrigo@gmail.com>
2022-05-31 16:37:33 -0400

    Add project files.

---
+++ FileRenamer/Program.cs
@@ -0,0 +1,11 @@
+﻿//Usage example FileRenamer E:\DCIM\100MEDIA\*.jpg -n DJI_ -f 0001 ==> DJI_0001.JPG
+
+namespace FileRenamer;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
