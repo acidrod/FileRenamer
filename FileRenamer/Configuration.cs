997222122a84c2d416419674796225d1f21c7d83
acidrod <alarcon.rodrigo@gmail.com>
2022-05-31 23:33:48 -0400

    Succesfully added all the required arguments and implemented the CommandLineParser.

---
+++ FileRenamer/Configuration.cs
@@ -0,0 +1,17 @@
+﻿namespace FileRenamer;
+
+internal static class Configuration
+{
+    internal static void RunOptions(Options opt)
+    {
+        //handle options
+        Console.WriteLine($"Path: {opt.FilesPath}");
+        Console.WriteLine($"Rename: {opt.ToFileName}");
+        Console.WriteLine($"Numeric: {opt.NumericStart}");
+    }
+
+    internal static void HandleParseError(IEnumerable<Error> errs)
+    {
+        //handle errors
+    }
+}
