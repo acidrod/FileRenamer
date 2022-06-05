cce7f09946a9da2c37cced451142aa6ba5f78635
acidrod <alarcon.rodrigo@gmail.com>
2022-06-05 03:35:08 -0400

    Changed FileService.GetFiles(string path) to throw ArgumentNullException when the argument is null or empty. Also, added the first set of tests over the FileService.GetFiles method.

---
+++ FileRenamer.Tests/FileServiceTests.cs
@@ -0,0 +1,59 @@
+﻿namespace FileRenamer.Tests;
+
+[TestFixture]
+public class FileServiceTests
+{
+    private string _currentDirectory;
+
+    public FileServiceTests()
+    {
+        _currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+    }
+
+
+    [TestCase]
+    public void When_PathExists_Expects_GetFilesNotNull()
+    {
+        //Arrange 
+
+        //Act
+        FileInfo[] current = FileService.GetFiles(_currentDirectory);
+
+        //Assert
+        Assert.That(current, Is.Not.Null);
+    }
+
+    [TestCase]
+    public void When_PathExists_Expects_GetFilesNotEmpty()
+    {
+        //Arrange
+
+        //Act
+        FileInfo[] current = FileService.GetFiles(_currentDirectory);
+
+        //Assert
+        Assert.That(current, Is.Not.Null);
+    }
+
+    [TestCase]
+    public void When_PathExists_Expects_GetFilesToHaveFileRenamerDllPresent()
+    {
+        //Arrange
+        FileInfo[] current = FileService.GetFiles(_currentDirectory);
+
+        //Act
+        var findDll = current
+            .Where(f => f.Name.Equals("FileRenamer.dll", StringComparison.CurrentCultureIgnoreCase));
+
+        //Assert
+        Assert.That(findDll, Is.Not.Null);
+    }
+
+    [TestCase("")]
+    [TestCase(null)]
+    public void When_PathIsNullOrEmpty_Expects_ArgumentNullException(string givenPath)
+    {
+        string path = givenPath;
+        Assert.Throws<ArgumentNullException>(() => FileService.GetFiles(path));
+    }
+}
